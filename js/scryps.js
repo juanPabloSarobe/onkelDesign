8993dc9341d1b5bb27ccfb875371118a482f355a
Juan Pablo Sarobe <jp.sarobe@gmail.com>
2022-10-27 16:35:32 -0300

    terminando los lofos de redes sociales

--- js/scryps.js
+++ js/scryps.js
@@ -12,9 +12,13 @@ window.onscroll = function() {
     if (y < currentY) {
       document.querySelector('#customNavbar').classList.remove('navbar-light', 'bg-opacity-10', 'fixed-theme');
       document.querySelector('#customNavbar').classList.add('navbar-dark', 'bg-dark', 'fixed-theme');
+      document.querySelector('#logoNavbar').classList.remove('logoDark');
+      document.querySelector('#logoNavbar').classList.add('logoLigth');
     } else if(y == currentY) {
         document.querySelector('#customNavbar').classList.remove('navbar-dark', 'bg-dark', 'fixed-theme');
       document.querySelector('#customNavbar').classList.add('navbar-light', 'bg-opacity-10', 'fixed-theme'); 
+      document.querySelector('#logoNavbar').classList.remove('logoLigth');
+      document.querySelector('#logoNavbar').classList.add('logoDark');
     }
   }
   
